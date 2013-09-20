b446b1492c9f6b35a650337bb7f299ae90155c76
MG <sonora_matancera@hotmail.es>
2013-09-20 14:51:06 +0200

    edit one to many

--- src/Test/MainBundle/Controller/FormController.php
+++ src/Test/MainBundle/Controller/FormController.php
@@ -69,6 +69,68 @@ class FormController extends Controller
             'form' => $form->createView()
         );
     }
+    
+    /**
+     * @Route("/forms/{id}/edit", name="forms_edit")
+     * @ParamConverter("factura", class="MainBundle:Factura")
+     * @Template()
+     */
+    public function editAction(User $factura, Request $request)
+    {
+        $originalProductos = array();
 
+        // Create an array of the current Producto objects in the database
+        foreach ($factura->getProductos() as $producto) {
+            $originalProductos[] = $producto;
+        }
+
+        $form = $this->createForm(new FacturaProductosType(), $factura);
+
+        if ($request->isMethod("POST")) {
+            $form->bind($request);
+
+            if ($form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+
+                // filter $originalProductos to contain adressess no longer present
+                foreach ($factura->getProductos() as $producto) {
+                    foreach ($originalProductos as $key => $toDel) {
+                        if ($toDel->getId() === $producto->getId()) {
+                            unset($originalProductos[$key]);
+                        }
+                    }
+                }
+
+                // remove the relationship between the producto and the Task
+                foreach ($originalProductos as $producto) {
+                    // remove the Producto from the Factura
+                    $factura->getProductos()->removeElement($producto);
+
+                    // if you wanted to delete the Producto entirely, you can also do that
+                    $em->remove($producto);
+                }
+
+                $em->persist($factura);
+                 $em->flush();
+
+                $flashBag = $this->get('session')->getFlashBag();
+                $flashBag->add('test_success', 'Se ha editado una Factura:');
+                $flashBag->add('test_success', sprintf('Descripcion: %s', $factura->getDescripcion()));
+                if (0 !== count($factura->getProductos())) {
+                    $flashBag->add('test_success', 'Direcciones:');
+                    foreach ($factura->getProductos() as $producto) {
+                        $flashBag->add('test_success', sprintf('&nbsp;&nbsp;%s', $producto->getNombre()));
+                    }
+                }
+
+                return $this->redirect($this->generateUrl('forms'));
+            }
+        }
+
+        return array(
+            'form' => $form->createView(),
+            'factura' => $factura,
+        );
+    }
     
 }
